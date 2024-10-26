99b6f6f42df7c0693268e3632acf42ca9c2066b5
Lauren Caballero <yurybenitez@192.168.80.19>
2024-10-25 20:16:25 -0500

    Se crea la tienda para la actividad de la clase de hoy 25-10

--- src/main/java/com/michael/market/controllers/VegetableController.java
+++ src/main/java/com/michael/market/controllers/VegetableController.java
@@ -15,27 +15,27 @@ public class VegetableController {
     @Autowired
     VegetableService VegetableService;
 
-    @GetMapping("/vegetables")
+    @GetMapping("/vegetable")
     public List<VegetableEntity> getVegetables() {
         return VegetableService.getVegetables();
     }
 
-    @GetMapping("/vegetables/{id}")
+    @GetMapping("/vegetable/{id}")
     public Optional<VegetableEntity> getByIdVegetables(@PathVariable String id) {
         return VegetableService.getVegetables(id);
     }
 
-    @PostMapping("/vegetables")
+    @PostMapping("/vegetable")
     public VegetableEntity createVegetables(@RequestBody VegetableEntity Vegetables) {
         return VegetableService.setVegetables(Vegetables);
     }
 
-    @PutMapping("/vegetables/{id}")
+    @PutMapping("/vegetable/{id}")
     public VegetableEntity updateVegetables(@PathVariable String id, @RequestBody VegetableEntity Vegetables) {
         return VegetableService.updateVegetables(id, Vegetables);
     }
 
-    @DeleteMapping("/vegetables/{id}")
+    @DeleteMapping("/vegetable/{id}")
     public Boolean deleteVegetables(@PathVariable String id) {
         return VegetableService.deleteVegetables(id);
     }
